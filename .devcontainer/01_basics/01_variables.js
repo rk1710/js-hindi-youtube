b03457c536b4d97d687fa7d656c002c78cb1f4f3
rk1710 <131847003+rk1710@users.noreply.github.com>
2025-10-07 09:07:12 +0000

    send to projectfile

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -2,7 +2,11 @@ const accountId = 144553
 let accountEmail = "rushi@google.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
+let accountState;
 
-accountId = 2
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
 
 console.log("accountId")
+console.table([accountId, accountEmail , accountPassword, accountCity, accountState])
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs , nor browser
+
+let name = "Rushi"
+let age = 21
+let isLoggedIn = false
+let state;
+// number => 2 to power 53
+// bigint
+// string => "" , '' , ``
+// boolean => true/false
+// null => standalone value
+// undefined => standalone value
+// symbol => unique
